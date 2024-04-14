42cd92e7c0d728dbe67458ed9bebcd82607b9dad
jorge <mendezjorge98@gmail.com>
2024-04-14 15:35:51 -0300

    Carpeta Resources a directorio raiz

---
+++ src/main/Cartas/Opcion.java
@@ -0,0 +1,39 @@
+package Cartas;
+
+import Estadisticas.Estadistica.NivelExcedidoException;
+import Estadisticas.Estadistica.NivelInvalidoException;
+import Juego.Personaje;
+
+public class Opcion {
+	
+	private String informacion;
+	private int niveles[];
+	
+	public Opcion() {
+		informacion = null;
+		niveles = null;
+	}
+	
+	public void modificarEstadisticas(Personaje personaje) throws NivelExcedidoException, NivelInvalidoException {
+		personaje.getEstadisticas().modificarEjercito(niveles[0]);
+		personaje.getEstadisticas().modificarIglesia(niveles[1]);
+		personaje.getEstadisticas().modificarOro(niveles[2]);
+		personaje.getEstadisticas().modificarPueblo(niveles[3]);
+	}
+	
+	public void setInformacion(String informacion) {
+		this.informacion = informacion;
+	}
+	
+	public String getInformacion() {
+		return informacion;
+	}
+	
+	public int[] getNiveles() {
+		return niveles;
+	}
+	
+	public void setNiveles(int niveles[]) {
+		this.niveles = niveles;
+	}
+}
