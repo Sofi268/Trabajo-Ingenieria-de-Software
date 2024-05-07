833895027eff1fba05e5cd6f9fe983356c6a622a
sofiavalos <sofi555sofi@gmail.com>
2024-05-07 13:09:05 -0300

    add +1 test en cartas

--- src/main/Test/CartaTest.java
+++ src/main/Test/CartaTest.java
@@ -3,11 +3,15 @@ package Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.Test;
 
 import Cartas.Carta;
 import Cartas.Opcion;
+import Estadisticas.Estadistica.NivelExcedidoException;
+import Estadisticas.Estadistica.NivelInvalidoException;
+import Juego.Personaje;
 
 public class CartaTest {
 
@@ -35,4 +39,27 @@ public class CartaTest {
         assertEquals("Opción A", carta.verOpcion(opcionA)); // Verifica que verOpcion() retorna la opción A
         assertEquals("Opción B", carta.verOpcion(opcionB)); // Verifica que verOpcion() retorna la opción B
     }
+    
+    public void testElegirOpcion() {
+        Carta carta = new Carta();
+        Personaje personaje = new Personaje();
+        // define un nivel inicial de las estadisticas
+        int nivelInicial = personaje.getEstadisticas().getNivelActual();
+        
+        // coloca valores arbitrarios en las opciones
+        int[] valoresOpcionA = {10, 20, 0, 0};
+        int[] valoresOpcionB = {0, 10, -10, 0};  
+
+        // intenta elegir una opcion
+        try {
+            carta.elegirOpcion(personaje, valoresOpcionA, valoresOpcionB, "A"); // Elige la opcion A
+        } catch (NivelExcedidoException | NivelInvalidoException e) {
+            fail("Excepcion: " + e.getMessage());
+        }
+        
+        // crea un entero para ver el nivel despues de elegir la opcion
+        int nivelModificado = personaje.getEstadisticas().getNivelActual();
+        assertEquals(nivelInicial + valoresOpcionA[0], nivelModificado); // Verifica que las estadisticas hayan cambiado conforme a la opcion
+    }
+
 }
