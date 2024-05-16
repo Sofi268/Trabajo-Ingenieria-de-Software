29d0e6449a91d9141d59cde0438a2cd18b21b30b
Sofia Castro <116833245+Sofi268@users.noreply.github.com>
2024-05-16 20:32:56 -0300

    Delete src/main/Main.java

--- src/main/Main.java
+++
@@ -1,8 +0,0 @@
-import Juego.Juego;
-import javafx.application.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        Application.launch(Juego.class, args);
-    }
-}
