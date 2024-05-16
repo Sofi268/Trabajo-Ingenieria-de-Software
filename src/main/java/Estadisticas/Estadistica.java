c42d48a26c7cd61ef4714b7132e246553e7d211e
Sofia Castro <116833245+Sofi268@users.noreply.github.com>
2024-05-16 20:29:56 -0300

    Add files via upload

---
+++ src/main/java/Estadisticas/Estadistica.java
@@ -0,0 +1,48 @@
+package Estadisticas;
+
+public class Estadistica {
+	private static final int NIVEL_MAXIMO = 100;
+	private static final int NIVEL_MINIMO = 0;
+	private static final int NIVEL_INICIAL = 50;
+
+    private int nivelActual;
+
+    public Estadistica() {
+        nivelActual = NIVEL_INICIAL;
+    }
+    
+    public void setNivelActual(int nivel) {
+    	nivelActual = nivel;
+    }
+    
+    public int getNivelActual() {
+    	return nivelActual;
+    }
+
+    public void ajustarNivel(int cantidad) throws NivelExcedidoException, NivelInvalidoException {
+        int nuevoNivel = nivelActual + cantidad;
+        if (nuevoNivel > NIVEL_MAXIMO) {
+            throw new NivelExcedidoException();
+        }
+        if (nuevoNivel < NIVEL_MINIMO) {
+            throw new NivelInvalidoException();
+        }
+        nivelActual = nuevoNivel;
+    }
+    
+    public class NivelExcedidoException extends Exception {
+		private static final long serialVersionUID = 1L;
+
+		public NivelExcedidoException() {
+            super();
+        }
+    }
+    
+    public class NivelInvalidoException extends Exception {
+		private static final long serialVersionUID = 1L;
+
+		public NivelInvalidoException() {
+            super();
+        }
+    }
+}
---
+++ src/main/java/Estadisticas/EstadisticasConjuntas.java
@@ -0,0 +1,60 @@
+package Estadisticas;
+
+import java.util.HashMap;
+
+public class EstadisticasConjuntas extends Estadistica {
+    private Estadistica agua;
+    private Estadistica aire;
+    private Estadistica tierra;
+    private Estadistica fuego;
+    
+    public EstadisticasConjuntas() {
+        agua = new Estadistica();
+        aire = new Estadistica();
+        tierra = new Estadistica();
+        fuego = new Estadistica();
+    }
+    
+    public void modificarAgua(int cantidad) throws NivelExcedidoException, NivelInvalidoException {
+        agua.ajustarNivel(cantidad);
+    }
+    
+    public void modificarAire(int cantidad) throws NivelExcedidoException, NivelInvalidoException {
+        aire.ajustarNivel(cantidad);
+    }
+    
+    public void modificarTierra(int cantidad) throws NivelExcedidoException, NivelInvalidoException {
+        tierra.ajustarNivel(cantidad);
+    }
+    
+    public void modificarFuego(int cantidad) throws NivelExcedidoException, NivelInvalidoException {
+        fuego.ajustarNivel(cantidad);
+    }
+    
+    public HashMap<String, Integer> getEstadisticasNiveles () {
+    	HashMap<String, Integer> estadisticasNiveles = new HashMap<String, Integer>();
+    	estadisticasNiveles.put("agua", agua.getNivelActual());
+    	estadisticasNiveles.put("aire", aire.getNivelActual());
+    	estadisticasNiveles.put("tierra", tierra.getNivelActual());
+    	estadisticasNiveles.put("fuego", fuego.getNivelActual());
+    	return estadisticasNiveles;
+    }
+    
+
+	public int getAgua() {
+		return agua.getNivelActual();
+	}
+
+	public int getAire() {
+		return aire.getNivelActual();
+	}
+
+	public int getTierra() {
+		return tierra.getNivelActual();
+	}
+
+	public int getFuego() {
+		return fuego.getNivelActual();
+	}
+
+}
